04f67dfe6ff72f485740ac29d50ecdb9f2cd3c42
smv1999 <vaidhyanathan.sm@gmail.com>
2020-09-27 01:38:51 +0530

    added type identification features to the Numbers.h

--- Numbers.h
+++ Numbers.h
@@ -1,5 +1,6 @@
 #include <math.h>
-
+#include <string>
+#include <type_traits>
 typedef long long int LLI;
 
 LLI reverse(LLI num);
@@ -7,7 +8,10 @@ int sumOfDigits(LLI num);
 bool isPrime(LLI num);
 int gcd(LLI num1, LLI num2);
 int lcm(LLI num1, LLI num2);
-
+int max(int num1, int num2);
+int min(int num1, int num2);
+template <typename T>
+std::string type(T ele);
 /*
 Reverse the digits of the number
 */
@@ -68,3 +72,41 @@ int lcm(LLI num1, LLI num2)
 {
     return (num1 * num2) / gcd(num1, num2);
 }
+
+/*
+Returns the max element among the two
+*/
+int max(int num1, int num2)
+{
+    return ((num1>num2)?(num1):(num2));
+}
+
+/*
+Returns the min element among the two
+*/
+int min(int num1, int num2)
+{
+    return ((num1<num2)?(num1):(num2));
+}
+
+/*
+Returns the type of the element given as parameter
+*/
+template <typename T>
+std::string type(T ele)
+{
+
+    if(std::is_integral<T>::value)
+        return "int";
+    else if(std::is_floating_point<T>::value)
+        return "float";
+    else if(std::is_array<T>::value)
+        return "array";
+    else if(std::is_class<T>::value)
+        return "class";
+    else if(std::is_function<T>::value)
+        return "function";
+    else if(std::is_pointer<T>::value)
+        return "pointer";
+}
+
