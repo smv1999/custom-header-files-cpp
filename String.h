96627fa5f89ea8f16801a5a403020fd9d0c2a558
Vaidhyanathan S M <vaidhyanathan.sm@gmail.com>
2021-05-05 16:02:25 +0530

    Done with the String Header File

--- String.h
+++ String.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <map>
 #include <cctype>
+#include <vector>
 
 // Tasks - https://www.w3schools.com/python/python_ref_string.asp
 
@@ -41,10 +42,10 @@ bool startswith(string s1, string s2, int start = 0, int end = 0)
 {
     if (start != 0)
     {
-        string temp = "";
+        string tempStr = "";
         for (int index = start; index < end; index++)
-            temp += s1[index];
-        return contains(temp, s2);
+            tempStr += s1[index];
+        return contains(tempStr, s2);
     }
     for (int index = 0; index < s2.length(); index++)
     {
@@ -63,10 +64,10 @@ bool endswith(string s1, string s2, int start = 0, int end = 0)
 {
     if (end != 0)
     {
-        string temp = "";
+        string tempStr = "";
         for (int index = start; index < end; index++)
-            temp += s1[index];
-        return startswith(temp, s2, temp.length() - s2.length(), s1.length() - 1);
+            tempStr += s1[index];
+        return startswith(tempStr, s2, tempStr.length() - s2.length(), s1.length() - 1);
     }
     // normal case
     return startswith(s1, s2, s1.length() - s2.length(), s1.length() - 1);
@@ -187,4 +188,190 @@ int findLastOccurence(string s, string val)
     }
 
     return lastIndex;
+}
+
+/*
+Returns true if all the characters in the string are alphanumeric
+*/
+bool isalnum(string s)
+{
+    for (int index = 0; index < s.length(); index++)
+    {
+        if (!isalnum(s[index]))
+            return false;
+    }
+    return true;
+}
+
+bool isalpha(string s)
+{
+    for (int index = 0; index < s.length(); index++)
+    {
+        if (!isalpha(s[index]))
+            return false;
+    }
+    return true;
+}
+
+bool isdigit(string s)
+{
+    for (int index = 0; index < s.length(); index++)
+    {
+        if (!isdigit(s[index]))
+            return false;
+    }
+    return true;
+}
+
+bool islower(string s)
+{
+    for (int index = 0; index < s.length(); index++)
+    {
+        if (!islower(s[index]))
+            return false;
+    }
+    return true;
+}
+
+bool isupper(string s)
+{
+    for (int index = 0; index < s.length(); index++)
+    {
+        if (!isupper(s[index]))
+            return false;
+    }
+    return true;
+}
+
+/*
+Returns true if the string follows the rules of a title
+*/
+bool istitle(string s)
+{
+
+    bool followsTitle = false;
+    if (isupper(s[0]))
+        followsTitle = true;
+    for (int index = 1; index < s.length(); index++)
+    {
+        if (s[index] == ' ')
+        {
+            if (isupper(s[index + 1]))
+                followsTitle = true;
+            else
+                followsTitle = false;
+        }
+    }
+
+    return followsTitle;
+}
+
+/*
+Converts the string passed as input into lowercase
+*/
+void lower(string &s)
+{
+    for (int index = 0; index < s.length(); index++)
+    {
+        s[index] = tolower(s[index]);
+    }
+}
+
+/*
+Converts the string passed as input into uppercase
+*/
+void upper(string &s)
+{
+    for (int index = 0; index < s.length(); index++)
+    {
+        s[index] = toupper(s[index]);
+    }
+}
+
+/*
+Splits the string at the specified separator and returns a vector of strings
+*/
+vector<string> split(string s, char c = ' ')
+{
+    vector<string> res;
+    string tempStr = "";
+    for (int index = 0; index < s.length(); index++)
+    {
+        if (s[index] != c)
+        {
+            tempStr += s[index];
+        }
+        else
+        {
+            res.push_back(tempStr);
+            tempStr = "";
+        }
+    }
+    res.push_back(tempStr);
+    return res;
+}
+
+/*
+Swaps cases in a string, i.e., lowercase becomes uppercase and vice-versa.
+*/
+void swapcase(string &s)
+{
+    for (int index = 0; index < s.length(); index++)
+    {
+        if (islower(s[index]))
+            s[index] = toupper(s[index]);
+        else
+            s[index] = tolower(s[index]);
+    }
+}
+
+/*
+Converts the first character of each word to uppercase
+*/
+void title(string &s)
+{
+    s[0] = toupper(s[0]);
+
+    for (int index = 1; index < s.length(); index++)
+    {
+        if (s[index] == ' ')
+        {
+            s[index + 1] = toupper(s[index + 1]);
+        }
+    }
+}
+
+/*
+Replaces the specified value with the specified value in the string
+*/
+
+void replace(string &s, char c, char k)
+{
+    for (int index = 0; index < s.length(); index++)
+    {
+        if (s[index] == c)
+        {
+            s[index] = k;
+        }
+    }
+}
+
+void replace(string &s, string c, string k)
+{
+    int replacePosition = find(s, c);
+    int replacerIndex = 0, index;
+    if (c.length() > k.length())
+    {
+        for (index = replacePosition; index < replacePosition + k.length(); index++)
+        {
+            s[index] = k[replacerIndex++];
+        }
+        s.erase(index, c.length() - k.length());
+    }
+    else
+    {
+        s.insert(replacePosition, k);
+
+        s.erase(replacePosition + k.length(), c.length());
+    }
 }
